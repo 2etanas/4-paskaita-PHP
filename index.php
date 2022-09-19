a118957d5fbe0af009337bdfdf6c03adc3e375f2
2etanas <105873056+2etanas@users.noreply.github.com>
2022-09-19 19:50:57 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+
+//GitHub
+//_SERVER
+// 2 namu darbas
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get" action="index.php">
+        <input name="skaitiklis">
+        <input name="vardiklis">
+        <input name="skaitiklis">
+        <input name="vardiklis">
+        
+
+</body>
+</html>
+
+<?php
+
+?>
